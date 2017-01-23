cce530f1b47ff41819b2d04e139ceba4ba410a90
René Kooi <renee@kooi.me>
2017-01-23 10:58:31 +0100

    Detect mobile browsers for server-side renders, closes #2
    This uses the browser user-agent to check for mobile browsers. If
    rendering on the server side, the result of the user-agent check
    is used; otherwise, the CSS media query with the mobile breakpoint
    is used.

--- components/MatchList.js
+++ components/MatchList.js
@@ -6,11 +6,21 @@ import MatchDesktop from './MatchDesktop'
 
 
 export default class MatchList extends React.Component {
+  static contextTypes = {
+    // Whether we're targeting a mobile device during a server-side render.
+    isMobileAgent: React.PropTypes.bool
+  }
+
   render () {
+    const { isMobileAgent } = this.context
     const { d, timezone, matches } = this.props
     return (
       <Media query={mobileBreakpoint}>
-        {isMobile => {
+        {isMobileViewport => {
+          // isMobileAgent is `null` on client renders. We should only use it
+          // for server renders.
+          const isMobile = isMobileAgent !== null ? isMobileAgent : isMobileViewport
+
           const MatchComponent = isMobile ? MatchMobile : MatchDesktop
           return (
             <div>
