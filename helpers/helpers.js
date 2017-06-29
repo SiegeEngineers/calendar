284e7d8be19eca92d0926eb7bd09a5ed824dc2c2
root <charles.offenbacher@gmail.com>
2017-06-29 15:19:21 -0400

    fixing lowercase bug

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,1 +1,4 @@
-export const normalize = (s) => (s.toLowerCase().replace('_','').replace('1','i').replace('3','e').replace('4','a').replace(' ', ''))
+export const normalize = (s) => {
+  if (!s) return ''
+  return s.toLowerCase().replace('_','').replace('1','i').replace('3','e').replace('4','a').replace(' ', '')
+}
